209cee26ccbbc847f9e9f6fc8c9a62dbe1f4fe96
loulabdev <loulab.dev@gmail.com>
2026-04-01 14:12:56 +0900

    Update useExamData.ts

--- src/hooks/useExamData.ts
+++ src/hooks/useExamData.ts
@@ -1,1 +1,12 @@
+import { useMemo } from 'react';
+import { ExamType } from '../types/exam';
+import { loadAllExamData } from '../utils/dataLoader';
 
+export const useExamData = (examType: ExamType) => {
+  const dataset = useMemo(() => loadAllExamData(examType), [examType]);
+
+  return {
+    theoryData: dataset.theory,
+    pastData: dataset.past,
+  };
+};
